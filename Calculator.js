4b690094cfe6f7aeaf1d4a671765459f1c4b2abf
Alex Randall <63251618+Alexander-Randall@users.noreply.github.com>
2020-10-27 12:43:22 -0500

    Uploaded files from VS code

---
+++ Calculator.js
@@ -0,0 +1,30 @@
+function clear(){
+    document.getElementById("output").innerHTML = "0";
+}
+
+function removeZero() {
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").innerHTML = value;
+    }
+}
+
+
+function percentage() {
+    var value = document.getElementById("output").innerHTML;
+    value = value / 100
+    document.getElementById("output").innerHTML = value;
+}
+
+function forDisplay(value) {
+    removeZero()
+    document.getElementById("output").innerHTML += value;
+}
+
+function solve() {
+    removeZero()
+    var equation = document.getElementById("output").innerHTML;
+    var solved = eval(equation);
+    document.getElementById('output').innerHTML = solved;
+}
